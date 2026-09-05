1e5c0acc747900a16030efff78f9fec14a8e38ac
saurav raj <saurav15092007@gmail.com>
2026-09-05 10:30:37 +0530

    first commit

---
+++ java script practice/app.js
@@ -0,0 +1,10 @@
+// const s = 100;
+// export default s
+
+import saa from './script.js'
+
+console.log(saa)
+
+document.querySelector('#hello').innerText = saa;
+
+document.querySelector('#hello').innerText = 'hello';
---
+++ java script practice/script.js
@@ -0,0 +1,11 @@
+// let h2 = document.createElement('h2')
+// console.log(h2);
+// h2.innerHTML= 'saurav raj'
+// document.body.appendChild(h2)
+
+// import s from './app.js'
+// console.log(s)
+
+let saa = 'saurav raj'
+
+export default saa
